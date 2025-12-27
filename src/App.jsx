f27927923ed0b73a25146dad58d65840e4efac7a
razalms <raza@lmsathena.com>
2025-12-27 12:00:09 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,16 @@ function App() {
 
   return (
     <>
+      
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Hello World</h1>
+        <p>This is a paragraph</p>
+        <div>this is chanign in the main branch</div>
+        <div>this is chanign in the main branch</div>
+        <div>this is chanign in the main branch</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          This is changed by raza. and want to see the changes in the github.
-        </button>
-        <p>
-        this is changed by raza. and want to see the changes in the github in main.
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
