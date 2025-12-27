abd772ac5d81e4d977d95a3ce13f80a152a7e546
razalms <raza@lmsathena.com>
2025-12-27 11:27:36 +0530

    checking the changes into the

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          This is changed by raza. and want to see the changes in the github.
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
